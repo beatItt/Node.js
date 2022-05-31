3bf8890d58b8a811ea4957a625a4deb0421b9bc2
Ruchi <ruchi02.work@gmail.com>
2022-05-31 10:33:27 +0530

    added 3rd party logger middleware morgan, used express middleware for static file serving

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express=require('express');
+var morgan=require('morgan');
+
 const app=express(); //app is instance of express app
+
 
 //register view engine
 app.set('view engine','ejs');//by default,ejs views looked up inside views folder, app.set('views',folder)
 //listen on port 3000
+
 app.listen(3000,()=>{
     console.log("listening on port 3000 for requests")
 });//localhost is auto inferred
+
+
+//middleware shipped with express, for allowing serving static files to browser
+//(by def, server protects all files on server)
+app.use(express.static('public'));//whatever inside public folder could be served to browser
+
+
+//logger middleware :morgan, with dev formatting /tiny
+app.use(morgan('dev'));
+//custom logger middleware
+app.use((req,res,next)=>{
+    console.log("path :", req.path);// like url
+    console.log("host :", req.hostname);
+    console.log("method :", req.method);
+    next()// without next() browser hangs , we need to move on express explicitly to next handler/middleware using next() cuz we are not sending response bk to browser
+//npm install --save package : no longer needed post npm 5 versions to add to package.json dep section, and not just locally
+});
 
 //handlers for requests
 //callbk fn fired when req comes
